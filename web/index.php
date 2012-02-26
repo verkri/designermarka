594b01a2c3d1205c25421f25157b3ff3c953ed21
Krisztian Veress <veresskrisztian@gmail.com>
2012-02-26 15:03:35 +0000

    introduced dispatcher index.php, ignored cache and log directories from svn

--- web/index.php
+++ web/index.php
@@ -1,7 +1,22 @@
 <?php
-
-
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+// get the domain parts as an array
+list($tld, $domain, $subdomain, $subdomain2) = array_reverse(explode('.', $_SERVER['HTTP_HOST']));
+
+// determine which subdomain we're looking at
+$env = ( $subdomain == 'dev' ) ? 'dev' : 'prod';
+$app = ( $subdomain == 'dev' ) ? $subdomain2 : $subdomain;
+$app = (empty($app) || $app == 'www' ) ? 'frontend' : $app;
+
+// determine which app to load based on subdomain
+if (!is_dir(realpath(dirname(__FILE__).'/..').'/apps/'.$app))
+{
+  $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', $env, false);
+}
+else
+{
+  $configuration = ProjectConfiguration::getApplicationConfiguration($app, $env, false);
+}
+
 sfContext::createInstance($configuration)->dispatch();
