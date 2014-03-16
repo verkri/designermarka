be38cecb2e2a6bbe2ef26b61a932bafe5ddfb1b1
kalzso <kalozi.zsolt@gmail.com>
2014-03-16 23:14:09 +0100

    Event related model files

---
+++ lib/model/doctrine/MarkaEventTable.class.php
@@ -0,0 +1,66 @@
+<?php
+
+class MarkaEventTable extends Doctrine_Table {
+
+  public static function getInstance() {
+    return Doctrine_Core::getTable('MarkaEvent');
+  }
+  
+  /*public function getEventsFiltered(MarkaCategory $category, MarkaColorScheme $cs) {
+    $q = $this->createQuery('p')
+            ->where('category_id = ?',$category->getId())
+            ->andWhere('colorscheme_id = ?',$cs->getId())
+            ->orderBy('cadprice DESC');
+    return $this->filterActiveEvents($q)->execute();
+  }*/
+  
+  public function getEventsFiltered(MarkaCategory $category, MarkaType $t) {
+    $q = $this->createQuery('p')
+            ->where('category_id = ?',$category->getId())
+            ->andWhere('type_id = ?',$t->getId())
+            ->orderBy('cadprice DESC');
+    return $this->filterActiveEvents($q)->execute();
+  }
+  
+  public function getFeatured($count = 8) {
+    $q = $this->createQuery('p')
+            ->where('featured = true')
+            ->orderBy('random()');            
+    return $this->filterActiveEvents($q)->limit($count)->execute();
+  }
+  
+  public function getFeaturedCount() {
+    $q = $this->createQuery('p')
+            ->where('featured = true');
+    return $this->filterActiveEvents($q)->count();
+  }
+  
+  public function getActiveEvents(Doctrine_Query $q = null) {
+    return $this->filterActiveEvents($q)->execute();
+  }
+  
+/*
+  public function retrieveActiveEvent(Doctrine_Query $q) {
+    return $this->filterActiveEvents($q)->fetchOne();
+  }
+
+  
+
+  public function countActiveEvents(Doctrine_Query $q = null) {
+    return $this->filterActiveEvents($q)->count();
+  }
+*/
+  
+  public function filterActiveEvents(Doctrine_Query $q = null) {
+    if (is_null($q)) {
+      $q = Doctrine_Query::create()
+              ->from('MarkaEvent m');
+    }
+
+    $alias = $q->getRootAlias();
+
+    $q->andWhere($alias . '.is_active = true');
+    return $q;
+  }
+
+}
