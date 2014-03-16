04b2b5cdbe6528451583de1f86655ab00eb0860f
kalzso <kalozi.zsolt@gmail.com>
2014-03-16 22:44:11 +0100

    adding/modifying elements on the event form (not finished)

--- apps/backend/modules/event/templates/_list_batch_actions.php
+++ apps/backend/modules/event/templates/_list_batch_actions.php
@@ -2,10 +2,6 @@
   <select name="batch_action">
     <option value=""><?php echo __('Choose an action', array(), 'sf_admin') ?></option>
     <option value="batchDelete"><?php echo __('Delete', array(), 'sf_admin') ?></option>
-    <option value="batchActivate"><?php echo __('Activate', array(), 'sf_admin') ?></option>
-    <option value="batchDeactivate"><?php echo __('Deactivate', array(), 'sf_admin') ?></option>
-    <option value="batchFeaturize"><?php echo __('Featurize', array(), 'sf_admin') ?></option>
-    <option value="batchUnfeaturize"><?php echo __('Unfeaturize', array(), 'sf_admin') ?></option>
   </select>
   <?php $form = new BaseForm(); if ($form->isCSRFProtected()): ?>
     <input type="hidden" name="<?php echo $form->getCSRFFieldName() ?>" value="<?php echo $form->getCSRFToken() ?>" />
--- apps/backend/modules/event/templates/_list_td_tabular.php
+++ apps/backend/modules/event/templates/_list_td_tabular.php
@@ -1,40 +1,6 @@
 <td class="sf_admin_text sf_admin_list_td_name">
   <?php echo link_to($marka_event->getName(), 'marka_event_edit', $marka_event) ?>
 </td>
-<td class="sf_admin_foreignkey sf_admin_list_td_category_id">
-  <?php echo $marka_event->getCategory()->getName() ?>
-</td>
-<td class="sf_admin_foreignkey sf_admin_list_td_type_id">
-  <?php echo $marka_event->getType()->getName() ?>
-</td>
-<td class="sf_admin_text sf_admin_list_td_stockqty">
-  <?php echo $marka_event->getStockqty() ?>
-</td>
-<td class="sf_admin_text sf_admin_list_td_cadprice">
-  $ <?php echo $marka_event->getCadprice() ?>.0 CAD
-</td>
-<td class="sf_admin_boolean sf_admin_list_td_featured">
-  <?php 
-    $options = null;
-    if ( $marka_event->getFeatured() ) {
-      $options = array ( 'value' => true, 'desc' => 'Featured.');
-    } else {
-      $options = array ( 'value' => false, 'desc' => 'Not featured', 'display_icon' => false);
-    }
-    echo get_partial('event/list_field_status', $options) 
-  ?>
-</td> 
-<td class="sf_admin_boolean sf_admin_list_td_is_active">
-  <?php 
-    $options = null;
-    if ( $marka_event->getIsActive() ) {
-      $options = array ( 'value' => true, 'desc' => 'Active');
-    } else {
-      $options = array ( 'value' => false, 'desc' => 'Inactive');
-    }
-    echo get_partial('event/list_field_status', $options) 
-  ?>
-</td>
 <td class="sf_admin_boolean sf_admin_list_td_imagecount">
   <?php 
     $options = null;
